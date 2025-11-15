61872bfd5868f67353b2d8243dec489cbdfd05c4
Clément Omnès <clement.mns.pro@gmail.com>
2025-11-15 11:22:44 +0100

    chore: init project

---
+++ src/main/java/com/arqetype/growli/GrowliApplication.java
@@ -0,0 +1,13 @@
+package com.arqetype.growli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowliApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GrowliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arqetype/growli/GrowliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arqetype.growli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
