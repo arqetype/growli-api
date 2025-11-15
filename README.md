1d6650a9c946f3c90274fe17d0b163f74b3fe3be
Clément Omnès <clement.mns.pro@gmail.com>
2025-11-15 12:18:57 +0100

    refactor: rename application and test classes, update README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Growli API
+
+A Spring Boot REST API.
+
+## Prerequisites
+
+- Java 21+
+- Maven 3.9+
+
+## Getting Started
+
+```
+mvn spring-boot:run
+```
+The API will be available at http://localhost:8080/
--- src/main/java/com/arqetype/growli/GrowliAPIApplication.java
+++ src/main/java/com/arqetype/growli/GrowliAPIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GrowliApplication {
+public class GrowliAPIApplication {
 
 	static void main(String[] args) {
-		SpringApplication.run(GrowliApplication.class, args);
+		SpringApplication.run(GrowliAPIApplication.class, args);
 	}
 
 }
